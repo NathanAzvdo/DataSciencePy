eaa6df4f31215331f776270f965869cbf13fed3d
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-07-03 18:42:44 -0300

    indexação e cortes de arrayas

---
+++ indexação_e_cortes.py
@@ -0,0 +1,16 @@
+import requests
+import pandas as pd
+from urllib.request import urlretrieve
+import matplotlib.pyplot as plt
+
+
+url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+
+urlretrieve(url, 'covid_cases.csv')
+
+DF_covid = pd.read_csv('covid_cases.csv')
+# print(DF_covid['Lat']) exibe apenas a coluna Lat
+print(DF_covid.drop(['Lat', 'Long'], axis=1, inplace=True))##Remove o Lat e Long, o inplace ele faz com que o DF original já seja atualizado dessa exclusão
+DF_covid.groupby('Country/Region').sum()##Agrupa pelo pais e soma número de casos de todas as linhas daquele pais
+
+print(DF_covid.groupby('Country/Region').sum().loc['Albania'])##Pega apenas a linha da albania
